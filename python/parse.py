c1e426f2a88d87fefd3130670886c36ca4a080c0
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2018-09-30 15:30:26 -0700

    filter by higher frequencies, and pretty print

--- python/parse.py
+++ python/parse.py
@@ -22,22 +22,60 @@ def print_messages(messages, target):
 def calc_frequency(messages):
     """
     return a frequency dictionary based on the list of messages
+    do not include empty strings
     """
-    freq_dict = {}
+    freq = {}
     for message in messages:
         words = message["content"].split(" ")
         for word in words:
-            if word in freq_dict:
-                freq_dict[word] += 1
-            else:
-                freq_dict[word] = 1
-    return freq_dict
+            if len(word) > 0:
+                if word in freq:
+                    freq[word] += 1
+                else:
+                    freq[word] = 1
+    return freq
+
+def filter_freq(freq, thresh):
+    """
+    help format the frequency dictionary of words
+    return a smaller set with a count >= thresh
+    along with the ratio of shrinking
+    """
+    if len(freq) ==  0:
+        return {}
+
+    filtered = {}
+    for key, val in freq.items():
+        if val >= thresh:
+            filtered[key] = val
+
+    ratio = len(filtered) / len(freq)
+    return filtered, ratio
+
+
+def pretty_print_freq(freq):
+    """
+    print with extra spaces a word frequency table
+    crop strings that are too long, and add spaces for short ones
+    """
+
+    TARGET = 10
+
+    print("================================")
+    for word, value in sorted(freq.items(), key=lambda(x): -1 *x[1]):
+        n = len(word)
+        if n <= TARGET:
+            print(word + ' ' * (TARGET - n) + " | " + str(value))
+    print("================================")
 
 
 if __name__== "__main__":
     print("begin parsing")
     data = read_data()
-    print_messages(data, 'J')
-    print_messages(data, 'W')
-    freq_dict = calc_frequency(data)
-    print(freq_dict)
+    freq = calc_frequency(data)
+    filtered, ratio = filter_freq(freq, 50)
+
+    print(filtered)
+    print("ratio: " + str(ratio))
+
+    pretty_print_freq(filtered)
