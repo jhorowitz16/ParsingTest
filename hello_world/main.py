06d6d27175eccb3f039ad7b0734250b5c63e774b
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2018-10-24 00:17:48 -0700

    rendering the template

--- hello_world/main.py
+++ hello_world/main.py
@@ -13,12 +13,16 @@
 # limitations under the License.
 
 import webapp2
-
+import os
+from google.appengine.ext.webapp import template
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         # self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('<h1>Hello friend</h1>')
+        # self.response.write('<h1>Hello friend</h1><input type="text"/><button>Submit</button>')
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, {}))
 
 
 app = webapp2.WSGIApplication([
