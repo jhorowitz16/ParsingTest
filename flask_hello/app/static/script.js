7326c6e49e0040210195e26ffbb25adb56ada1b9
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2018-11-06 22:29:46 -0800

    push to try to deploy to gcloud

--- flask_hello/app/static/script.js
+++ flask_hello/app/static/script.js
@@ -15,10 +15,39 @@ function checkAnswer(guess) {
     document.total += 1;
   }
   setScoreText();
+  makeAnswerVisible();
+  disableButtons();
 }
 
+function next() {
+  makeAnswerInvisible();
+  enableButtons();
+}
+
+
+/* ============================================= */
+/* ================== HELPERS ================== */
+/* ============================================= */
 
 function setScoreText() {
   const text = document.correct + " / " + document.total;
   document.getElementById("score").innerText = text;
+}
+
+function makeAnswerVisible() {
+  document.getElementById("answer").style.display = "block";
+}
+
+function makeAnswerInvisible() {
+  document.getElementById("answer").style.display = "none";
+}
+
+function disableButtons() {
+  document.getElementById("button--J").disabled = true;
+  document.getElementById("button--W").disabled = true;
+}
+
+function enableButtons() {
+  document.getElementById("button--J").disabled = false;
+  document.getElementById("button--W").disabled = false;
 }
