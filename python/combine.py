c6ab872ceaa46f3a61017d18768fa2abb8c54599
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2018-12-02 21:35:25 -0800

    add base combine file

---
+++ python/combine.py
@@ -0,0 +1,18 @@
+FILENAME_BASE = "../../data/message-10-31.json"
+FILENAME_EXTRA = "../../data/message-12-2.json"
+
+
+def combine(base, extra):
+    """
+    read both files
+    get the final timestamp on the first list
+    find which message has that timestamp
+    construct the combined version
+    write to csv
+    """
+    pass
+    print(base, extra)
+
+
+if __name__== "__main__":
+    combine(FILENAME_BASE, FILENAME_EXTRA)
